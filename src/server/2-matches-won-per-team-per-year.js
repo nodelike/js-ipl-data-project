60f94daef02df51081596c8ce05d223fc49ecb32
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-08 17:21:14 +0530

    Fixing the add the matches won per team per year instead of calculating total wins of each team.

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -7,12 +7,16 @@ function getMatchesWonPerTeamPerYear(matches){
   try {
     let wins = {}
     for(let match of matches){
-      if(wins[match.winner] === undefined){
-          wins[match.winner] = 1;
-      } else {
-          wins[match.winner] += 1;
+      if(wins[match.season] === undefined){
+        wins[match.season] = {};
       }
-    }
+      
+      if(wins[match.season][match.winner] === undefined){
+        wins[match.season][match.winner] = 1;
+      } else {
+        wins[match.season][match.winner] += 1;
+      }
+  }
     return wins;
   } catch (error) {
     console.log(`Error counting number of wins per year for each team. ${error}`);
