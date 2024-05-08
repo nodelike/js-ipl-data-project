880247515242abaa245fbd74f4b1c3e898bc4ab5
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-08 16:52:22 +0530

    Implemented the function and the output file of the result in output folder to get matches per year.

---
+++ src/server/1-matches-per-year.js
@@ -0,0 +1,23 @@
+const getData = require('../data/data.js');
+const saveDataToJSON = require("./saveDataToJSON.js");
+
+let matches = getData().matches();
+
+function getMatchesPerYear(matches){
+  try {
+    let matchesInYear = {}
+    for(let match of matches){
+      if(matchesInYear[match.season] === undefined){
+          matchesInYear[match.season] = 1;
+      } else {
+          matchesInYear[match.season] += 1;
+      }
+    }
+    return matchesInYear;
+  } catch (error){
+    console.log(`Error counting number of matches per year. ${error}`);
+  }
+}
+
+let result = getMatchesPerYear(matches)
+saveDataToJSON(result, "matchesPerYear.json");
