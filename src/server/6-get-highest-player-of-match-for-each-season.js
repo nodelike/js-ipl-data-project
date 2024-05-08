bb9e9d12f8651819e80f793cc002cd35df91f7fc
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-09 01:23:35 +0530

    Refactored the code to get the player with highest player of match per season using array/object methods.

--- src/server/6-get-highest-player-of-match-for-each-season.js
+++ src/server/6-get-highest-player-of-match-for-each-season.js
@@ -5,38 +5,38 @@ let matches = getData().matches();
 
 function getHighestPlayerOfMatchForEachSeason(matches){
   try {
-    let playerOfMatchCounts = {}
-    for(let match of matches){
-        if(playerOfMatchCounts[match.season] == undefined){
-            playerOfMatchCounts[match.season] = {};
-        } else {
-            if(playerOfMatchCounts[match.season][match.player_of_match] == undefined){
-                playerOfMatchCounts[match.season][match.player_of_match] = 1;
-            } else {
-                playerOfMatchCounts[match.season][match.player_of_match] += 1;
-            }
-        }
-    }
-    
-    let highestPlayerOfMatchPerSeason = {}
-    for (let season in playerOfMatchCounts) {
-        let highestPlayerOfMatch = '';
-        let highestPlayerOfMatchCount = 0;
-  
-        for (let player in playerOfMatchCounts[season]) {
-          if (playerOfMatchCounts[season][player] > highestPlayerOfMatchCount) {
-            highestPlayerOfMatch = player;
-            highestPlayerOfMatchCount = playerOfMatchCounts[season][player];
-          }
-        }
-  
-        highestPlayerOfMatchPerSeason[season] = highestPlayerOfMatch;
+    let playerOfMatchCounts = {};
+    matches.forEach( match => {
+      if(playerOfMatchCounts[match.season] == undefined){
+        playerOfMatchCounts[match.season] = {};
       }
+      
+      if(playerOfMatchCounts[match.season][match.player_of_match] == undefined){
+          playerOfMatchCounts[match.season][match.player_of_match] = 1;
+      } else {
+          playerOfMatchCounts[match.season][match.player_of_match] += 1;
+      }
+    });
+    
+    let highestPlayerOfMatchPerSeason = {};
+    Object.entries(playerOfMatchCounts).forEach( ([season, playerCounts]) => {
+      let highestPlayerOfMatch = '';
+      let highestPlayerOfMatchCount = 0;
+
+      Object.entries(playerCounts).forEach( ([player, count]) => {
+        if (count > highestPlayerOfMatchCount) {
+          highestPlayerOfMatch = player;
+          highestPlayerOfMatchCount = count;
+        }
+      });
+
+      highestPlayerOfMatchPerSeason[season] = highestPlayerOfMatch;
+    });
     
     // console.log(highestPlayerOfMatchPerSeason);
     return highestPlayerOfMatchPerSeason;
   } catch (error) {
-    console.log(`Error counting number of wins per year for each team. ${error}`);
+    console.log(`Error getting the player with highest player of match per season. ${error}`);
   }
 }
 
