cc4501c21d69d4b9c6510f611b64a2fcfab26f06
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-09 01:21:03 +0530

    Refactored the code to get number of wins per year for each team using array/object methods.

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -6,17 +6,19 @@ let matches = getData().matches();
 function getMatchesWonPerTeamPerYear(matches){
   try {
     let wins = {}
-    for(let match of matches){
+
+    matches.forEach( match => {
       if(wins[match.season] === undefined){
         wins[match.season] = {};
       }
-      
+
       if(wins[match.season][match.winner] === undefined){
         wins[match.season][match.winner] = 1;
       } else {
         wins[match.season][match.winner] += 1;
       }
-  }
+    });
+
     return wins;
   } catch (error) {
     console.log(`Error counting number of wins per year for each team. ${error}`);
