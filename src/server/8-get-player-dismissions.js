7669578a42a2affdd55c3030c0df1df04f77ab83
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-08 17:15:34 +0530

    Implemented the function and the output file of the result in output folder to get highest player dismissed pair and the count.

---
+++ src/server/8-get-player-dismissions.js
@@ -0,0 +1,44 @@
+const getData = require('../data/data.js');
+const saveDataToJSON = require("./saveDataToJSON.js");
+
+let deliveries = getData().deliveries();
+
+function getHighestPlayerDismissions(deliveries){
+  try {
+    let playerDismissalCount= {};
+    
+    for(let delivery of deliveries){
+        if(delivery.player_dismissed && delivery.bowler && delivery.dismissal_kind){
+            const dismissalKey = `${delivery.bowler}-${delivery.player_dismissed}`
+
+            if(dismissalKey in playerDismissalCount){
+                playerDismissalCount[dismissalKey]++;
+            } else {
+                playerDismissalCount[dismissalKey] = 1;
+            }
+        }
+    }
+
+    let highestPlayerDismissalCount = 0;
+    let highestPlayerDismissalPairs =''
+    for(let dismissalPairs in playerDismissalCount){
+        if(playerDismissalCount[dismissalPairs] > highestPlayerDismissalCount){
+            highestPlayerDismissalCount = playerDismissalCount[dismissalPairs];
+            highestPlayerDismissalPairs = dismissalPairs;
+        }
+    }
+
+    const result = {
+        bowler: highestPlayerDismissalPairs.split("-")[0],
+        playerDismissed: highestPlayerDismissalPairs.split("-")[1],
+        dismissalTimes: highestPlayerDismissalCount
+    };
+    return result;
+  } catch (error) {
+    console.log(`Error counting number of extra runs per team in year 2016. ${error}`);
+  }
+}
+
+let result = getHighestPlayerDismissions(deliveries);
+console.log(result);
+saveDataToJSON(result, "highestPlayerDismissed.json");
