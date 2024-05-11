945bfe8d94adb6730d9d6f6c2fc9c49c64b47a98
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-11 11:41:22 +0530

    Implemented the function getMatchesPerTeamPerSeason using methods that displays the number of matches won per team per season.

---
+++ src/server/11-get-matches-won-per-team-per-season.js
@@ -0,0 +1,29 @@
+const getData = require('../data/data.js');
+const saveDataToJSON = require("./saveDataToJSON.js");
+
+let matches = getData().matches();
+
+function getMatchesWonPerTeamPerSeason(matches){
+  try {
+    let wins = matches.reduce( (accumulator, match) => {
+        if(accumulator[match.season] == undefined){
+            accumulator[match.season] = {}
+        }
+
+        if(accumulator[match.season][match.winner] == undefined){
+            accumulator[match.season][match.winner] = 1
+        } else {
+            accumulator[match.season][match.winner] += 1
+        }
+
+        return accumulator
+    }, {});
+
+    return wins;
+  } catch (error) {
+    console.log(`Error counting number of wins per year for each team. ${error}`);
+  }
+}
+
+let result = getMatchesWonPerTeamPerSeason(matches);
+saveDataToJSON(result, "matchesWonPerTeamPerSeason.json");
